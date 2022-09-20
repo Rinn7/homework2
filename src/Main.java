336b28719343c0ae665dea4f8d75e3ece7b75395
LENOVO <frisli.089.df@gmail.com>
2022-09-20 16:44:44 +0300

    Переменная cat

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         var dog = 8;
         System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
     }
 }
